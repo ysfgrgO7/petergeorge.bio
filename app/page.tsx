a556a1ac0da7793eea8ebe7424246c56b0ae1ca0
Youssef George <ysfgrg2007@gmail.com>
2025-08-07 05:52:52 +0300

    Start Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,42 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { FaInstagram, FaFacebook, FaYoutube, FaTiktok } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h1> You bring the dream ... We bring the Way </h1>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>
+          Integrated Sciences
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/science.svg"
+            alt="Integrated Sciences"
+            width={250}
+            height={250}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          First Secondary
+        </button>
+        <button className={styles.button}>
+          Biology
+          <Image src="/heart.svg" alt="Biology" width={250} height={250} />
+          Third Secondary
+        </button>
+        <button className={styles.button}>
+          Geology
+          <Image src="/earth.svg" alt="Geology" width={250} height={250} />
+          Third Secondary
+        </button>
+      </div>
+
+      <div className={styles.socialMedia}>
+        <FaFacebook className={styles.icon} />
+        <FaInstagram className={styles.icon} />
+        <FaYoutube className={styles.icon} />
+        <FaTiktok className={styles.icon} />
+      </div>
+
     </div>
   );
 }
