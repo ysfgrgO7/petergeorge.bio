ca6e0d3adecf2745918e399d1edb83bba1416879
Youssef <ysfgrg2007@gmail.com>
2025-10-07 03:49:10 +0300

    This is for the env variables for firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCd7s5hEmcJPU_6B0nUAcBgEUO0BnbkjFU",
-  authDomain: "master-biology.firebaseapp.com",
-  projectId: "master-biology",
-  storageBucket: "master-biology.firebasestorage.app",
-  messagingSenderId: "191148972070",
-  appId: "1:191148972070:web:400fcacb1d1335a731eee7",
-  measurementId: "G-XG45EP0R5F",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
